2b0aa6877667ec29a5a0548c6d80849a1395249b
ccwTMS <ccw.thomas@gmail.com>
2015-04-15 22:55:49 +0800

    ** Add -l option to show real path when it is symbolic link file.    It's available when -q option is enabled.    This option can prevent infinite loop when -r option is enabled.

--- cateye.py
+++ cateye.py
@@ -11,11 +11,12 @@ def cateye_usage():
 	print "usage: cateye.py [path] [OPTION]"
 	print ""
 	print "[OPTION]:"
-	print "-d seconds\t\t: update regularly at interval of seconds, where the \"seconds\" can be float point number. "
-	print "-s parameters\t\t: write parameters to file. use \"\" when parameters more than one."
-	print "-q\t\t\t: quiet, show filename only, no content of file."
-	print "-r\t\t\t: recursively works in folder for list function. (likes tree)"
-	print "-h,--help\t\t: shows this usage."
+	print "-d seconds    : update regularly at interval of seconds, where the \"seconds\" can be float point number. "
+	print "-h,--help     : It shows this usage."
+	print "-l            : To show symbolic link's real path, and to prevent infinite loop.(available when -q option is enabled.)"
+	print "-r            : It recursively works in folder for list function. (likes tree)"
+	print "-s parameters : To write parameters to file. use \"\" when parameters more than one."
+	print "-q            : quiet, It shows filename only, no content of file."
 	print ""
 
 def dols(path="."):
@@ -50,6 +51,7 @@ def cateye(ctl, basefolder="/sys"):
 		indent+="    "
 
 	for leaf in dols(basefolder):
+		info=""
 		fullleaf = os.path.join(basefolder,leaf)
 		if os.path.isfile(fullleaf):
 			ctl["f_type"]='f'
@@ -58,14 +60,35 @@ def cateye(ctl, basefolder="/sys"):
 		else:
 			ctl["f_type"]='s'
 
+		ctl["l_path"]=""
 		if os.path.islink(fullleaf):
 			ctl["f_type"] = ctl["f_type"].upper()
+			ctl["l_path"] = os.path.realpath(fullleaf)
 
 
-		print(indent + "\033[1m" + " [" + ctl["f_type"] + "] " + leaf +(ctl['q'] and "\033[0m" or ": "+"\033[0m"+docat(fullleaf)))
+		info = indent + "\033[1m" + " [" + ctl["f_type"] + "] " + leaf 
 
-		if ctl['r'] == 1:
+		if ctl['q']:
+			if ctl['l'] and os.path.islink(fullleaf):
+				info += ": " + "\033[0m" + ctl["l_path"]
+			else:
+				info += "\033[0m"
+		
+		else:
 			if os.path.isdir(fullleaf):
+				info += "\033[0m"
+			else:
+				info += ": " + "\033[0m" + docat(fullleaf)
+		
+		print(info)
+	##	print(indent + "\033[1m" + " [" + ctl["f_type"] + "] " + leaf +(ctl['q'] and "\033[0m"+(ctl['l'] and ": "+ ctl["l_path"] or "") or ": "+"\033[0m"+docat(fullleaf)))
+
+		
+		if ctl['r']:
+			if ctl['l'] and os.path.islink(fullleaf):
+				pass
+
+			elif os.path.isdir(fullleaf):
 				ctl["rec_cnt"]+=1;
 				cateye(ctl, fullleaf)
 				ctl["rec_cnt"]-=1;
@@ -80,13 +103,13 @@ def err_exit(err, ret_code, show_usage=False):
 
 if __name__ == "__main__":
 	try:
-		opts, args = getopt.gnu_getopt(sys.argv[1:], "d:hs:qr", ["help",])
+		opts, args = getopt.gnu_getopt(sys.argv[1:], "d:hls:qr", ["help",])
 	except getopt.GetoptError as err:
 		err_exit(err, 2, True)
 	
 	regular_time=0
 	sval=""
-	ctl={"q":0, "r":0, "rec_cnt":0, 'f_type':'f'}
+	ctl={"q":0, "r":0, "l":0, "rec_cnt":0, "f_type":'f', "l_path":""}
 	for op, ar in opts:
 		if op == "-d":
 			try:
@@ -102,6 +125,8 @@ if __name__ == "__main__":
 			ctl['q']=1
 		elif op == "-r":
 			ctl['r']=1
+		elif op == "-l":
+			ctl['l']=1
 			
 
 	while True:
